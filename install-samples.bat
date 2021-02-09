357fe564f462cc449ff9896e2d94fb691d467f73
Mark Steffensen <mark.steffensen@microfocus.com>
2021-02-09 22:47:45 +0000

    Updated to add installer script

---
+++ install-samples.bat
@@ -0,0 +1,11 @@
+# This file will install the various pull-xxx-samples for use in training and demo purposes.
+# This script and the other files should be placed into C:\tools\pull-samples
+# then run this bat file to put the other files in their correct places.
+#
+# This sample also exists on the public admpresales git hub so they could also be cloned from 
+# https://github.com/admpresales/
+
+copy ./pull-analysis-samples.bat C:\Users\demo\Documents\Analysis
+copy ./pull-scenrio-samples.bat C:\Users\demo\Documents\Controller\scenario
+copy ./pull-uft-samples.bat C:\Users\demo\Documents\Unified Functional Testing
+copy ./pull-vugen-samples.bat C:\Users\demo\Documents\VuGen\Scripts
